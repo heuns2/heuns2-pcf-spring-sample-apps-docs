5e8a2acdca85fd54989f88fb71d1984ed3bcc8b2
leedh <leedh@kuberix.com>
2022-01-05 18:03:18 +0900

    git commit

--- spring-cloud-gateway-k8s/src/main/java/gateway/Application.java
+++ spring-cloud-gateway-k8s/src/main/java/gateway/Application.java
@@ -15,9 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
 @RestController
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
