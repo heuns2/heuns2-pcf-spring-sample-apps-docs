7629257e768656a6baff8752242fe9b8234e44f6
leedh <leedh@kuberix.com>
2022-01-06 14:08:53 +0900

    git commit

--- gateway/src/main/java/test/kuberix/gateway/GatewayApplication.java
+++ gateway/src/main/java/test/kuberix/gateway/GatewayApplication.java
@@ -10,14 +10,12 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class GatewayApplication {
 	
 	@Autowired
